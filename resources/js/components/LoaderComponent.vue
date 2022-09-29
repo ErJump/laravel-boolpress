a4a695145866becaaf3cbcbcd37a4a72a6f0808d
ErJump <fedelijunior@gmail.com>
2022-09-29 13:53:22 +0200

    style: :lipstick: Adds style to loader component

--- resources/js/components/LoaderComponent.vue
+++ resources/js/components/LoaderComponent.vue
@@ -1,6 +1,11 @@
 <template>
-    <div>
-        <h1>Loading...</h1>
+    <div class="d-flex justify-content-center align-items-center w-100">
+        <div class="outer">
+            <div class="loader">
+                <span></span>
+                <p>Loading</p>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -10,6 +15,126 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+    /* animation thanks to Junaid Amin Bhat */
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
 
+    body {
+        background: #332233;
+        width: 100%;
+        min-height: 100vh;
+        display: grid;
+        place-items: center;
+    }
+
+    .loader {
+        width: 130px;
+        height: 130px;
+        background: #121212;
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+        border-radius: 8px;
+    }
+
+    .loader span {
+        display: inline-block;
+        width: 40px;
+        height: 40px;
+        background: #FFF;
+        border-radius: 50%;
+        margin: 0 60px 0px 0;
+        box-shadow: 60px 0px 0 #FFF;
+        position: relative;
+    }
+
+    .loader span:after {
+        content: '';
+        width: 100%;
+        height: 100%;
+        background: #000;
+        position: absolute;
+        left: 0;
+        top: 0;
+        border-radius: 50%;
+        box-shadow: 60px 0px 0 #000;
+        animation: closeEyes 15s linear infinite;
+    }
+
+    @keyframes closeEyes {
+
+        0%,
+        2%,
+        4%,
+        49%,
+        51%,
+        50%,
+        79%,
+        81%,
+        100% {
+            height: 25%;
+        }
+
+        3%,
+        50%,
+        80% {
+            height: 100%;
+        }
+    }
+
+    .loader span:hover:after {
+        height: 100%;
+        transition: 0.4s;
+    }
+
+    .loader span::before {
+        content: '';
+        height: 20px;
+        width: 20px;
+        border-radius: 50%;
+        background: #000;
+        position: absolute;
+        bottom: 6px;
+        left: 3px;
+        box-shadow: 62px 0 0 #000;
+        animation: moveEyes 2s linear alternate infinite;
+    }
+
+    @keyframes moveEyes {
+        to {
+            left: 12px;
+        }
+    }
+
+    p {
+        flex: 0 0 100%;
+        color: #FFF;
+        font-family: sans-serif;
+        text-transform: uppercase;
+        font-weight: 600;
+        text-align: center;
+        letter-spacing: 3px;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: rgba(255, 255, 255, 0.1);
+        background-image: linear-gradient(to left, transparent, #FF0, #f50, #FF0, transparent);
+        background-repeat: no-repeat;
+        background-size: 200%;
+        background-position: -100%;
+        animation: loadingText 2s linear alternate infinite;
+    }
+
+    @keyframes loadingText {
+        from {
+            background-position-x: 200%;
+        }
+
+        to {
+            background-position-x: -100%;
+        }
+    }
 </style>
